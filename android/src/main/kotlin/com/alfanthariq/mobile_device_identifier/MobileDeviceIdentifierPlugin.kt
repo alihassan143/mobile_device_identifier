d48195b8ce465592fae98de08442c1f7b1f71fe2
alihassan143 <alihassan143cool@gmail.com>
2025-05-29 14:23:41 +0500

    fix anr when getting id or some devices

--- android/src/main/kotlin/com/alfanthariq/mobile_device_identifier/MobileDeviceIdentifierPlugin.kt
+++ android/src/main/kotlin/com/alfanthariq/mobile_device_identifier/MobileDeviceIdentifierPlugin.kt
@@ -32,14 +32,18 @@ class MobileDeviceIdentifierPlugin: FlutterPlugin, MethodCallHandler {
     channel.setMethodCallHandler(null)
   }
 
-  private fun getDeviceId(): String? {
-      val wideVineUuid = UUID(-0x121074568629b532L, -0x5c37d8232ae2de13L)
-      return try {
-          val wvDrm = MediaDrm(wideVineUuid)
-          val wideVineId = wvDrm.getPropertyByteArray(MediaDrm.PROPERTY_DEVICE_UNIQUE_ID)
-          wideVineId.joinToString(":") { String.format("%02X", it) }
-      } catch (e: java.lang.Exception) {
-          null
-      }
-  }
+    private fun getDeviceId(): String? {
+        val wideVineUuid = UUID(-0x121074568629b532L, -0x5c37d8232ae2de13L)
+        return try {
+            if (!MediaDrm.isCryptoSchemeSupported(wideVineUuid)) {
+                return null
+            }
+            val wvDrm = MediaDrm(wideVineUuid)
+            val wideVineId = wvDrm.getPropertyByteArray(MediaDrm.PROPERTY_DEVICE_UNIQUE_ID)
+            wvDrm.close()  // always good to release
+            wideVineId.joinToString(":") { String.format("%02X", it) }
+        } catch (e: Exception) {
+            null
+        }
+    }
 }
