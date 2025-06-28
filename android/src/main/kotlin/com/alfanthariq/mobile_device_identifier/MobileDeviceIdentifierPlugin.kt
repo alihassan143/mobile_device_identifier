1a556a8516c7b497590fd38608eedd44cb353c1c
alihassan143 <alihassan143cool@gmail.com>
2025-06-28 15:22:48 +0500

    fixes anr and other changes

--- android/src/main/kotlin/com/alfanthariq/mobile_device_identifier/MobileDeviceIdentifierPlugin.kt
+++ android/src/main/kotlin/com/alfanthariq/mobile_device_identifier/MobileDeviceIdentifierPlugin.kt
@@ -1,49 +1,68 @@
 package com.alfanthariq.mobile_device_identifier
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.provider.Settings
 import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
-
-import android.media.MediaDrm
 import java.util.UUID
+import androidx.core.content.edit
+import io.flutter.Log
 
-class MobileDeviceIdentifierPlugin: FlutterPlugin, MethodCallHandler {
-  private lateinit var channel : MethodChannel
+class MobileDeviceIdentifierPlugin : FlutterPlugin, MethodChannel.MethodCallHandler {
+    private lateinit var channel: MethodChannel
+    private lateinit var appContext: Context
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "mobile_device_identifier")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getDeviceId") {
-      val devId = getDeviceId()
-      result.success(devId)
-    } else {
-      result.notImplemented()
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "mobile_device_identifier")
+        channel.setMethodCallHandler(this)
+        appContext = flutterPluginBinding.applicationContext
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 
-    private fun getDeviceId(): String? {
-        val wideVineUuid = UUID(-0x121074568629b532L, -0x5c37d8232ae2de13L)
-        return try {
-            if (!MediaDrm.isCryptoSchemeSupported(wideVineUuid)) {
-                return null
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        when (call.method) {
+            "getDeviceId" -> {
+                val deviceId = getDeviceId(appContext)
+                Log.i("decvice_id",deviceId);
+                result.success(deviceId)
             }
-            val wvDrm = MediaDrm(wideVineUuid)
-            val wideVineId = wvDrm.getPropertyByteArray(MediaDrm.PROPERTY_DEVICE_UNIQUE_ID)
-            wvDrm.close()  // always good to release
-            wideVineId.joinToString(":") { String.format("%02X", it) }
-        } catch (e: Exception) {
-            null
+            else -> result.notImplemented()
         }
+    }
+
+    @SuppressLint("HardwareIds")
+    private fun getDeviceId(context: Context): String {   return try {
+        val androidId = Settings.Secure.getString(context.contentResolver,Settings.Secure.ANDROID_ID)
+        Log.i("device_id from setting secure",androidId);
+        return androidId.ifEmpty {
+            getPersistentDeviceId(context)
+        }
+    } catch (e: Exception) {
+        // Log.e("MobileDeviceIdentifier", "Error fetching ANDROID_ID", e)
+        getPersistentDeviceId(context)
+    }
+    }
+
+    private fun getPersistentDeviceId(context: Context): String {    return try {
+        val sharedPref = context.getSharedPreferences("device_prefs", Context.MODE_PRIVATE)
+        var deviceId = sharedPref.getString("persistent_device_id", null)
+
+        if (deviceId == null) {
+            deviceId = UUID.randomUUID().toString()
+            sharedPref.edit { putString("persistent_device_id", deviceId) }
+        }
+
+        return deviceId
+    }catch (e: Exception) {
+        // Log.e("MobileDeviceIdentifier", "Error generating persistent ID", e)
+        // As a last resort fallback
+        UUID.randomUUID().toString()
+    }
     }
 }
